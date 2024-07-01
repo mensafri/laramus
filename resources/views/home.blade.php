944400668e45110815491c6109404439c1b697fd
ramaFE <suryarama4@gmail.com>
2024-07-02 01:00:50 +0700

    Contribute dan Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -170,6 +170,43 @@
                 </div>
             </div>
         </section>
+        <section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+            <h2>Ready to contribute?</h2>
+            <p class="mb-4">Want to make a big impact?</p>
+            <div class="text-center">
+                <a href="#" class ="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+                <a href="#" class ="btn btn-secondary mb-2 mb-lg-0">Join Discussions</a>
+            </div>
+        </section>
+        <footer class="py-80px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                       <img src="{{ url('assets/images/logo.png' )}}" alt="Logo" class="h-32px mb-4"> 
+                        <p class="text-white">Empowering Laramus<br/>to connect share join and learn</p>
+                    </div>
+                    <div class="col-12 col-lg-6 me-auto">
+                        <div class="d-flex flex-column flex-lg-row justify-content-end">
+                            <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                            <p class="fw-bold fs-5 text-white text-nowrap">
+                                Contact Us
+                            </p>
+                            <ul class="list unstyled">
+                               <li class="text-white"><a href ="mailto:hi@laramus.com">hi@laramus.com</a>
+                            </ul>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <p class="fw-bold fs-5 text-white">Links</p>
+                                <ul class="list unstyled">
+                                    <li class="text-white mb-2"><a href ="#">Home</a></li>
+                                    <li class="text-white mb-2"><a href ="#">Discussions</a></li>
+                                    <li class="text-white mb-2"><a href ="#">About Us</a></li>
+                                 </ul>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </footer>
 
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
